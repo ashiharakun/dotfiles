ea1ac725ca4b8a3d13948d1389afb9495c51d491
ashiharakun <github.reconcile469@passmail.net>
2026-08-04 20:58:52 +0900

    nix-hazkeyのflake inputを追加
    fcitx5向け日本語入力メソッドHazkeyを導入するための下準備。

--- flake.nix
+++ flake.nix
@@ -22,6 +22,10 @@
       url = "github:nix-community/NixOS-WSL/main";
       inputs.nixpkgs.follows = "nixpkgs-nixos";
     };
+    nix-hazkey = {
+      url = "github:aster-void/nix-hazkey";
+      inputs.nixpkgs.follows = "nixpkgs-nixos";
+    };
   };
 
   outputs =
